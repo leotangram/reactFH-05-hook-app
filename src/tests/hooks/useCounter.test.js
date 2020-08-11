da7342f3645bf675a95f95c4d182cb8cfa582d6c
Leonardo Omaña <leotangram@gmail.com>
2020-08-11 10:42:06 -0500

    Ejecutar funciones de cooktomHook dentro de las pruebas

--- src/tests/hooks/useCounter.test.js
+++ src/tests/hooks/useCounter.test.js
@@ -1,11 +1,48 @@
-import { renderHook } from '@testing-library/react-hooks'
+import { renderHook, act } from '@testing-library/react-hooks'
 import { useCounter } from '../../hooks/useCounter'
 
 describe('useCounter', () => {
   test('should return default values', () => {
     const { result } = renderHook(() => useCounter())
+    expect(result.current.counter).toBe(10)
     expect(typeof result.current.increment).toBe('function')
     expect(typeof result.current.decrement).toBe('function')
     expect(typeof result.current.reset).toBe('function')
+  })
+
+  test('should counter 100', () => {
+    const { result } = renderHook(() => useCounter(100))
+    expect(result.current.counter).toBe(100)
+  })
+
+  test('should increment the counter in 1', () => {
+    const { result } = renderHook(() => useCounter(100))
+    const { increment } = result.current
+    act(() => {
+      increment()
+    })
+    const { counter } = result.current
+    expect(counter).toBe(101)
+  })
+
+  test('should decrement the counter in 1', () => {
+    const { result } = renderHook(() => useCounter(100))
+    const { decrement } = result.current
+    act(() => {
+      decrement()
+    })
+    const { counter } = result.current
+    expect(counter).toBe(99)
+  })
+
+  test('counter should be 100', () => {
+    const { result } = renderHook(() => useCounter(100))
+    const { decrement, reset } = result.current
+    act(() => {
+      decrement()
+      reset()
+    })
+    const { counter } = result.current
+    expect(counter).toBe(100)
   })
 })
